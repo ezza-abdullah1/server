1876d4815e8dd49ed925b7b3cd383063115d85ef
ezza-abdullah1 <ezza.abdullahh@gmail.com>
2025-05-09 12:17:05 +0500

    modules

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const { createServer } = require('@vercel/node');
 
-// creating the express app
 const app = express();
 
-// defining a simple route
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
-// exporting the server as a Vercel handler
 module.exports = app;
